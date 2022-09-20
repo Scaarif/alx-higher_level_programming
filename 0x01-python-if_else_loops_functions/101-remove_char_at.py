df2ea1a1a21d22079f617842a20c5cb188bb1731
scaarifintl <scaarifintl@gmail.com>
2022-09-20 18:58:52 +0300

    fix2

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     """ Returns a copy of str with the character at n removed """
-    return str[:n] + str[n + 1:]
+    if n < 0:
+        return str
+    else:
+        return str[:n] + str[n + 1:]
