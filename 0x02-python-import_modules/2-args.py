5da37766561fbe2df4f42d21e292830a8d7eb03f
scaarifintl <scaarifintl@gmail.com>
2022-09-22 13:43:49 +0300

    fix pycodestyle error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} {}".format(len(sys.argv) - 1,
-        "arguments" if len(sys.argv) != 2 else "argument"), end="")
+    print("{} ".format(len(sys.argv) - 1), end="")
+    if len(sys.argv) != 2:
+        print("arguments", end="")
+    else:
+        print("argument", end="")
     print("{}".format("." if len(sys.argv) == 1 else ":"))
     for index, arg in enumerate(sys.argv):
         if index != 0:
