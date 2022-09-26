09abc5e62dbb7c2e212ab4260006a1cfc0f13217
scaarifintl <scaarifintl@gmail.com>
2022-09-26 12:10:25 +0300

    add up only the first 2 elements in tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,8 +5,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a: first tuple
         tuple_b: second tuple
     Description:
-        adds the elements at same indices in the two tuples together
-        substitutes for an empy element with 0 in either tuple
+        adds the first 2 elements at same indices in the two tuples together
+        substitutes for an empty element with 0 in either tuple
     Returns:
         a tuple of the result of summation
     """
@@ -21,8 +21,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (tuple_a[0], 0)
     elif len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
-    len_ = len(tuple_a) if len(tuple_a) > len(tuple_b) else len(tuple_b)
-    for i in range(len_):
+    # Add up only the first wo elements in the tuples
+    for i in range(2):
         sum_ = (tuple_a[i] + tuple_b[i])
         ans.append(sum_)
     return ans[0], ans[1]
