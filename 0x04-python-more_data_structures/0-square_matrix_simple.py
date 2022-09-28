761da7320cf7b1fe9f684632695b3e44d73f5ff7
scaarifintl <scaarifintl@gmail.com>
2022-09-28 21:22:37 +0300

    fix 0-sq...

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,6 +6,9 @@ def square_matrix_simple(matrix=[]):
     Return:
         the new squares matrix
     """
-    if len(matrix[0]):
-        return [list(map(lambda x: x**2, row)) for row in matrix]
-    return matrix
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
