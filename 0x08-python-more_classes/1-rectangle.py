1d7f1d2dcfb5c2e2488853cb356354841846503a
scaarifintl <scaarifintl@gmail.com>
2022-10-17 19:59:17 +0300

    ensure width and height are strictly ints (remove floats) in 1-rect...

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     def width(self, value):
         """ setter for the width attribute """
         # check that value is an integer & positive
-        if type(value) not in [int, float]:
+        if type(value) not in [int]:
             raise TypeError('width must be an integer')
         elif value < 0:
             raise ValueError('width must be >=0')
@@ -35,7 +35,7 @@ class Rectangle:
     def height(self, value):
         """ setter for the height attribute """
         # check that value is an integer & positive
-        if type(value) not in [int, float]:
+        if type(value) not in [int]:
             raise TypeError('height must be an integer')
         elif value < 0:
             raise ValueError('height must be >=0')
