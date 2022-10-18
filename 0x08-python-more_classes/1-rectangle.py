a3fdb835c9e87cc1d4477e569ef0fa48a8c0d43f
scaarifintl <scaarifintl@gmail.com>
2022-10-18 09:48:47 +0300

    word count fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
         if type(value) not in [int, float]:
             raise TypeError('width must be an integer')
         elif value < 0:
-            raise ValueError('width must be >=0')
+            raise ValueError('width must be >= 0')
         else:
             self.__width = int(value)
 
@@ -38,6 +38,6 @@ class Rectangle:
         if type(value) not in [int, float]:
             raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError('height must be >=0')
+            raise ValueError('height must be >= 0')
         else:
             self.__height = int(value)
