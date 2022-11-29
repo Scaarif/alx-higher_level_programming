e75f25ed0acf86625b316295ff84cf0fd7ec58ce
scaarifintl <scaarifintl@gmail.com>
2022-11-29 21:55:11 +0300

    order results by number

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -4,4 +4,4 @@
 -- 	the number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
 -- The database name will be passed as an argument of the mysql command
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
