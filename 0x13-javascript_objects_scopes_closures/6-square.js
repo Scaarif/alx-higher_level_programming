7cfa6b0960e0b9220c8a35ddf72ad4cc8d9c63ea
scaarifintl <scaarifintl@gmail.com>
2022-12-20 21:00:03 +0300

    actually extend Square in 6-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-/* A class Square that defines a square, initializing instance attribute size and inheriting from Rectangle */
-const Rectangle = require('./4-rectangle'); // import Rectangle into module
-class Square extends Rectangle {
-  // include a constructor (initializer) - is a function
-  constructor (size) { // initialize the instance via parent class
-    super(size, size);
-  }
+/* Extends the class, Square */
+const Square = require('./5-square'); // import Rectangle into module
 
-  charPrint (letter = 'X') {
-    for (let i = 0; i < this.height; i++) { console.log(letter.repeat(this.width)); }
-  }
-}
+Square.prototype.charPrint = function (letter = 'X') {
+  for (let i = 0; i < this.height; i++) { console.log(letter.repeat(this.width)); }
+};
 // make the class available outside module
 module.exports = Square;
