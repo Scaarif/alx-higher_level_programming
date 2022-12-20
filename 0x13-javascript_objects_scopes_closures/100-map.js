0aab4d8b0b1a398a377b2343cf50ce45c70dd71a
scaarifintl <scaarifintl@gmail.com>
2022-12-20 21:19:00 +0300

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,4 @@
 /* Imports an array and computes a new array */
 const list = require('./100-data');
 console.log(list);
-const list2 = list.map((x, index) => x * index);
-console.log(list2);
+console.log(list.map((x, index) => x * index));
