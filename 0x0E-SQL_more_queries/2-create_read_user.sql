7f242711449ed689940a324e640e7c45b1ac2847
scaarifintl <scaarifintl@gmail.com>
2022-12-30 18:39:11 +0300

    grant SELECT rights to user2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,4 +7,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grant the user only SELECT privileges in the database, hbtn_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
