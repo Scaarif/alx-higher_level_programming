7d80ecbd0f3e68db89cd526558c94bb3829c6936
scaarifintl <scaarifintl@gmail.com>
2023-01-16 23:03:18 +0700

    include content-type header in 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and filename as rguments, sends a POST request to URL and displays the body of the response
-curl -sfd @"$2" "$1"
+curl -sfd @"$2" -H "Content-Type: application/json" "$1"
