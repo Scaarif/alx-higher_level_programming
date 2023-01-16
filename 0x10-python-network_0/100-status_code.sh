63fe43356cc1e38b6b7dee599d86316a6c5de67d
scaarifintl <scaarifintl@gmail.com>
2023-01-16 23:10:47 +0700

    --

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL passed as an argument, sends a request to the URL and displays only the status code of the response
-curl -so /dev/null -w "%{http_code}\n" "$1"
+curl -so /dev/null -w "%{http_code}" "$1"
