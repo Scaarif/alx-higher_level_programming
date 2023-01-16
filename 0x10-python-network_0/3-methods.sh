ab07fe473fc00fc563486b105fac10ae9f6cccf8
scaarifintl <scaarifintl@gmail.com>
2023-01-16 22:49:58 +0700

    get only the header

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes a URL as first argument and displays all HTTPS methods the server will accept
-curl -sIX "OPTIONS" "$1" | grep 'Allow' | cut -d ':' -f2
+curl -sIX "OPTIONS" "$1" | grep 'Allow' | cut -d ' ' -f 2-
